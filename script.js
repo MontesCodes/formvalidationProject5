a4f6f7373f4cde0ebcf1dc7677efd37cde568390
MontiesCodes_LT-1557 <MontiesWebDev@Gmail.com>
2024-05-02 06:34:31 -0400

    updated HTML/JS time 22:16

---
+++ script.js
@@ -0,0 +1,6 @@
+const email = document.getElementById('email'),
+  password = document.getElementById('password'),
+  emailErr = document.getElementById('emailError'),
+  passErr = document.getElementById('passwordError'),
+  logInArea = document.getElementById('logInArea'),
+  logIn = document.getElementById('logIn');
