99101f1b387dd41428ea2a99f7221c2f2d8ec431
MontiesCodes_LT-1557 <MontiesWebDev@Gmail.com>
2024-05-03 12:49:47 -0400

    updated HTML/JS time 28:03

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const email = document.getElementById('email'),
-  password = document.getElementById('password'),
-  emailErr = document.getElementById('emailError'),
-  passErr = document.getElementById('passwordError'),
-  logInArea = document.getElementById('logInArea'),
-  logIn = document.getElementById('logIn');
+// const email = document.getElementById('email'),
+//   password = document.getElementById('password'),
+//   emailErr = document.getElementById('emailError'),
+//   passErr = document.getElementById('passwordError'),
+//   logInArea = document.getElementById('logInArea'),
+//   logIn = document.getElementById('logIn');
+
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const emailErr = document.getElementById('emailError');
+const passErr = document.getElementById('passwordError');
+const logInArea = document.getElementById('logInArea');
+const logIn = document.getElementById('logIn');
 
 function checkFieldEmpty(e) {
   if (e.value != '') {
@@ -16,8 +23,12 @@ function checkFieldEmpty(e) {
 function checkEmail() {
   let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
   if (email.value.match(pattern)) {
+    email.setAttribute('style', 'border-bottom: 2px solid green');
   } else {
+    email.setAttribute('style', 'border-bottom: 2px solid red');
   }
 }
 function checkPassword() {}
-function doValidate() {}
+function doValidate() {
+  checkEmail();
+}
